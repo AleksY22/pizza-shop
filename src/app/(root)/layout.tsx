c41f7031d740e4571df7d52199b76b91923dbf58
AleksY22 <71137788+AleksY22@users.noreply.github.com>
2026-01-02 16:58:20 +0300

    fix-one

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Header } from "@/shared/components/header";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Pizza",
@@ -15,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <main className="min-h-screen">
-      <Header hasSearch={true} hasCart={true} />
+      <Suspense>
+        <Header hasSearch={true} hasCart={true} />
+      </Suspense>
       {children}
       {modal}
     </main>
--- src/shared/components/checkout/checkout-address-form.tsx
+++ src/shared/components/checkout/checkout-address-form.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import { AddressInput, ErrorText, WhiteBlock } from "@/shared/components";
 import { FormTextarea } from "../form/form-textarea";
