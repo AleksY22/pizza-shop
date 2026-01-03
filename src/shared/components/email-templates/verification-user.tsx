94d0a6839db9e1e3b62190c8ffa54ad09e2f07d4
AleksY22 <71137788+AleksY22@users.noreply.github.com>
2026-01-03 19:40:21 +0300

    fix twelve verification

--- src/shared/components/email-templates/verification-user.tsx
+++ src/shared/components/email-templates/verification-user.tsx
@@ -9,7 +9,7 @@ export function VerificationUserTemplate({ code }: Props) {
         Код подтверждения: <h2>{code}</h2>
       </p>
       <p>
-        <a href={`http://localhost:3000/api/auth/verify?code=${code}`}>
+        <a href={`${process.env.MAIN_URL}/api/auth/verify?code=${code}`}>
           Подтвердить регистрацию
         </a>
       </p>
